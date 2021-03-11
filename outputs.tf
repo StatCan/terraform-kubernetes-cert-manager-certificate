ea415e7bb28b9d2223559545371fbc89e3949631
Zachary Seguin <zachary.seguin@canada.ca>
2021-03-11 18:31:12 +0000

    feat(outputs): Output secret name

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "name" {
   value = var.name
 }
 
+output "secret_name" {
+  value = "${var.name}-tls"
+}
+
 output "namespace" {
   value       = var.namespace
   description = "The namespace the identity resides."
